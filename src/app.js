3f6e1e54a9a41d5e2afd93e6d54c0840f6bb2c27
joel <yoelmenacho123@gmail.com>
2025-07-23 12:34:37 -0400

    first commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+import express from 'express';
+
+dotenv.config();
+
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: "Creación de servidor listo"})
+})
+
+app.listen(port, () => {
+    console.log(`listening on port: ${port}`)
+})
